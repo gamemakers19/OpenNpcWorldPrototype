bd5c9cbf715b50664297c2204dc078718fbd8f3b
Pavel418 <pavel@vvv.az>
2021-02-24 19:28:58 +0400

    running behavior

---
+++ Assets/Scripts/Npcs/RunningBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningBehavior : MonoBehaviour, IAttackable
+{
+    public NPC npc;
+    private void Awake()
+    {
+        npc = GetComponent<NPC>();
+    }
+    public void OnAttack(GameObject attacker, Attack attack)
+    {
+        npc.attacked = true;
+    }
+}
